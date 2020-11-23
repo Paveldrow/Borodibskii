1e5602d7c24f6866fb05dc165ed4d650d4209991
Pavel Drow <paveldrow@yandex.ru>
2020-11-23 03:51:33 +0300

    Добавление js для открытия окна-логина и окна с картой

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var login = document.querySelector(".login-link");
+var adress = document.querySelector(".contacts-button-map");
+var popupLogin = document.querySelector(".modal-login");
+var popupMap = document.querySelector(".modal-map");
+var closeLogin = document.querySelector(".modal-login-close");
+var closeMap = document.querySelector(".modal-map-close");
+
+login.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupLogin.classList.add("modal-show");
+});
+
+adress.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.add("modal-show");
+});
+
+closeLogin.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupLogin.classList.remove("modal-show");
+});
+
+closeMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("modal-show");
+});
